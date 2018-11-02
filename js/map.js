c6a35c3aa829111f01a66feffcca6914d1645f97
Michael Sinclair <ihaveknowledge@yahoo.co.uk>
2018-11-02 22:31:25 +0000

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,8 +1,12 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+var geojsonLayer = new L.GeoJSON.AJAX("geojson/LondonWardsSPI.geojson");       
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+
+geojsonLayer.addTo(mymap);
